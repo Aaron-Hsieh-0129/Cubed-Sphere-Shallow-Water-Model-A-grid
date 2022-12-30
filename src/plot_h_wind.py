b887fec5b635f33eff3e7552b8efea3919e29569
Aaron Hsieh <b08209006@ntu.edu.tw>
2022-12-30 16:33:55 +0800

    Modify plot code

--- src/plot_h_wind.py
+++ src/plot_h_wind.py
@@ -21,6 +21,7 @@ if __name__ == '__main__':
         print("finish1")
 
     try:
+        nProc = multiprocessing.cpu_count() - 4
         with Pool(nProc) as p:
             results = [p.apply_async(pf.plotOnSphereWindMul, (t, )) for t in range(0, 1000, 10)]
             final = [result.get() for result in results]
@@ -28,6 +29,7 @@ if __name__ == '__main__':
         print("finish2")
 
     try:
+        nProc = multiprocessing.cpu_count() - 4
         with Pool(nProc) as p:
             results = [p.apply_async(pf.plotSphereWindCartopy, (t, )) for t in range(0, 1000, 10)]
             final = [result.get() for result in results]
