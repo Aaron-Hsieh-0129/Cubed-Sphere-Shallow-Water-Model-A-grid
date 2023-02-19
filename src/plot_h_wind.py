44848059d88a43ee1bbff3eb639c4bbfc2ce39f7
Aaron@39 <b08209006@ntu.edu.tw>
2023-02-20 07:57:57 +0800

    Clear files

--- src/plot_h_wind.py
+++ src/plot_h_wind.py
@@ -21,15 +21,7 @@ if __name__ == '__main__':
     try:
         nProc = int(multiprocessing.cpu_count() / 2)
         with Pool(nProc) as p:
-            results = [p.apply_async(pf.plotOnSphereWindMul, (t, )) for t in range(0, to, leap)]
-            final = [result.get() for result in results]
-    except:
-        print("finish2")
-
-    try:
-        nProc = int(multiprocessing.cpu_count() / 2)
-        with Pool(nProc) as p:
             results = [p.apply_async(pf.plotSphereWindCartopy, (t, )) for t in range(0, to, leap)]
             final = [result.get() for result in results]
     except:
-        print("finish3")
+        print("finish2")
