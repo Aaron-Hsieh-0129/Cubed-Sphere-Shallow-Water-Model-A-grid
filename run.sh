39047e6d9f456c9d805f705a0fd64156fbf07fc0
Aaron@39 <b08209006@ntu.edu.tw>
2023-03-14 02:04:33 +0800

    delete plot in run.sh

--- run.sh
+++ run.sh
@@ -3,25 +3,3 @@
 rm -rf build outputs
 mkdir build
 cd build/ && cmake ../ && make && ./csswm
-
-cd ../src && python plot_h_wind.py && python plot_zeta.py
-
-cd ../graphs/h/curvilinear
-ffmpeg -r 3 -i %d.png -pix_fmt yuv420p h+wind.mov -y
-ffmpeg -r 3 -i %d.png h+wind.gif -y
-
-cd ../sphere
-ffmpeg -r 3 -i %d.png -pix_fmt yuv420p h+wind.mov -y
-ffmpeg -r 3 -i %d.png h+wind.gif -y
-
-cd ../sphere_cartopy
-ffmpeg -r 3 -i %d.png -pix_fmt yuv420p h+wind.mov -y
-ffmpeg -r 3 -i %d.png h+wind.gif -y
-
-cd ../../zeta/curvilinear
-ffmpeg -r 3 -i %d.png -pix_fmt yuv420p zeta.mov -y
-ffmpeg -r 3 -i %d.png zeta.gif -y
-
-cd ../sphere_cartopy
-ffmpeg -r 3 -i %d.png -pix_fmt yuv420p zeta.mov -y
-ffmpeg -r 3 -i %d.png zeta.gif -y
